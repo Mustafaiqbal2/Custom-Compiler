d7f4f336b77ac980dca9e1d81999191798696cf3
musta <musta@G15>
2025-02-19 00:23:29 +0500

    -----

--- Compiler/src/Compiler/Main.java
+++ Compiler/src/Compiler/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
         String filename = "code.ms";
         try {
+        	////
             // Get the absolute path for the source file
             Path sourcePath = Paths.get("src", "Compiler", filename);
             
